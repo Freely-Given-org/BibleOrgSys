38fa73ea9592c6b4e075592f7a2cca1c446128fd
Samuel Hunt <samuelhuntnz@gmail.com>
2026-04-10 21:07:23 +1200

    create abbreviate function

--- Rust/bos-internals/src/lib.rs
+++ Rust/bos-internals/src/lib.rs
@@ -96,6 +96,6 @@ pub use indexes::{
 };
 pub use markers::ExtraType;
 pub use parsing::{
-    UsfmFigureAttributes, WordWithAttributes, get_small_leading_int, parse_figure_attributes,
-    parse_word_attributes,
+    UsfmFigureAttributes, WordWithAttributes, abbreviate, get_small_leading_int,
+    parse_figure_attributes, parse_word_attributes,
 };
